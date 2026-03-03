5d899850a39e2ddb9328cec948aa74518e217d96
Roket <okuhlexesibe0@gmail.com>
2026-03-03 19:01:43 +0000

    Test allocation , add expr , it works

--- jit_rs/src/main.rs
+++ jit_rs/src/main.rs
@@ -1,4 +1,6 @@
 
+#![allow(dead_code, unused_imports)]
+
 mod lexer;
 mod parser;
 mod interpreter;
@@ -10,5 +12,18 @@ fn main() {
     let buf = codegen::compile("2 + 3;");
     let result = executor::execute(&buf);
     println!("result: {}", result); // should print 5
+
+    let buf = codegen::compile("x = 10; x + 3;");
+    let result = executor::execute(&buf);
+    println!("result: {}", result); // should print 13
+    
+
+    let buf = codegen::compile("x = 10; y = 7; x + y;");
+    let result = executor::execute(&buf);
+    println!("result: {}", result); // should print 17
+ 
+    
+
+
 }
 
